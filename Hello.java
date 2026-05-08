8bfa0ebbaaba65b3e2b9e3c955e83daaeedc5f6e
chrvnixx <emmanueladekola91@gmail.com>
2026-05-08 17:35:02 +0100

    loops

--- Hello.java
+++ Hello.java
@@ -2,30 +2,17 @@ class Hello
 {
     public static void main(String args[])
     {
-       int n = 3 ;
+      
+      
+        // while (n++ < 50)
+        // {
+        //     System.out.println("Hi" + n);
+        // }
 
-       switch(n){
-        case 1:
-            System.out.println("monday");
-            break;
-        case 2:
-            System.out.println("tuesday");
-            break;
-        case 3:
-            System.out.println("wednesday");
-            break;
-        case 4:
-            System.out.println("thursday");
-            break;
-        case 5:
-            System.out.println("friday");
-            break;
-        case 6:
-            System.out.println("saturday");
-            break;
-        case 7:
-            System.out.println("sunday");
-            break;
-       }
+        for(int n=1;n<5;n++)
+        {
+            System.out.println("Hi" + n);
+
+        }
     }
 }
