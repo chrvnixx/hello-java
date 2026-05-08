5f118eec8f7db5e624791706aeff4f4fa7313c0d
chrvnixx <emmanueladekola91@gmail.com>
2026-05-08 15:08:36 +0100

    first commit

---
+++ Hello.java
@@ -0,0 +1,18 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+       int numb1 = 10;
+       byte br = 90;
+
+       br = (byte)numb1;
+       numb1 = br;
+
+       float jan = 14.9f;
+
+       double x = jan;
+
+
+       System.out.println(x);
+    }
+} 
