d53d49e4e3a667c34a002dcdf4a2ad3adf0cb68f
chrvnixx <emmanueladekola91@gmail.com>
2026-05-08 16:12:17 +0100

    logical operators

--- Hello.java
+++ Hello.java
@@ -1,18 +1,16 @@
-class Hello
+class Hello 
 {
-    public static void main(String a[])
+    public static void main(String args[])
     {
-       int numb1 = 10;
-       byte br = 90;
+        int x =800;
+        int y = 610;
+        int z = 95;
 
-       br = (byte)numb1;
-       numb1 = br;
-
-       float jan = 14.9f;
-
-       double x = jan;
-
-
-       System.out.println(x);
+        if(x > y && x > z)
+            System.out.println(x);
+        else if(y > x && y > z)
+            System.out.println(y);
+        else 
+            System.out.println(z);
     }
-} 
+}
