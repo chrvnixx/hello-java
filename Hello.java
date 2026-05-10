2dd3ecf3b6784c0ec975c2f2133d8091ba51d0eb
chrvnixx <emmanueladekola91@gmail.com>
2026-05-10 21:02:19 +0100

    arrays

--- Hello.java
+++ Hello.java
@@ -1,31 +1,37 @@
-class Calculator
-{
-    int num =10;
-    public int add(int n1, int n2)
-    {
-        return n1 + n2;
-    }
-
-    public int add(int n1, int n2, int n3)
-    {
-        return n1 + n2 * n3;
-    }
-}
-
-
 public class Hello
 {
     public static void main(String a[])
     {
-        int num1 = 50;
-        int num2 = 73;
-        int num3 = 89;
-        Calculator calc = new Calculator();
-        Calculator calc1 = new Calculator();
+        int nums[][] = new int[6][];
+        
+        nums[0] = new int[2];
+        nums[1] = new int[9];
+        nums[2] = new int[7];
+        nums[3] = new int[4];
+        nums[4] = new int[8];
+        nums[5] = new int[6];
+
+        for(int i=0;i<6;i++)
+        {
+            for(int j=0;j<nums[i].length;j++)
+            {
+                nums[i][j] = (int)Math.floor(Math.random() * 10);
+            }
+           
+        }
+
+        for(int m[]:nums)
+        {
+            for(int n : m)
+                System.out.print(n + " ");
+
+            System.out.println();
+        }
+           
+            
 
 
-        int result = calc.add(num1, num2, num3);
-
-        System.out.println(calc1.num);
+            
+       
     }
 }
