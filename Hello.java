640784998d181fce63e555e5fd524c9cae3b3a6d
chrvnixx <emmanueladekola91@gmail.com>
2026-05-15 16:53:20 +0100

    Static

--- Hello.java
+++ Hello.java
@@ -1,15 +1,42 @@
+class Mobile
+{
+    String brand;
+    int price;
+    static String name;
+
+    public void show()
+    {
+        System.out.println(brand + " " + name + " : " + price);
+    }
+
+    public static void show1(Mobile phone1)
+    {
+        System.out.println(phone1.brand + " " + name + " : " + phone1.price);
+    }
+}
 
 
 public class Hello
 {
     public static void main(String a[])
     {
-    
-       String name = "sinatra";
-       name = name + " frank";
-        System.out.println("hello " + name);
-        
+        Mobile phone1 = new Mobile();
+        phone1.brand = "apple";
+        phone1.price = 1200;
+        Mobile.name = "smartphone";
 
+        Mobile phone2 = new Mobile();
+        phone2.brand = "samsung";
+        phone2.price = 1200;
+        Mobile.name = "smartphone";
+
+        Mobile phone3 = new Mobile();
+        phone3.brand = "google pixel";
+        phone3.price = 1200;
+        Mobile.name = "smartphone";
+
+        phone1.show();
+
+        Mobile.show1(phone1);
     }
-
 }
