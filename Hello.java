d45ed802108b5a7bd6d5dfbe7a78d8a81d3576f2
chrvnixx <emmanueladekola91@gmail.com>
2026-05-16 21:20:31 +0100

    contructors

--- Hello.java
+++ Hello.java
@@ -3,6 +3,18 @@ class Human
     private String name;
     private int age;
 
+    public Human()
+    {
+        name = "sinatra";
+        age = 18;
+    }
+
+    public Human(int a, String n)
+    {
+        age = a;
+        name = n;
+    }
+
     public void  setName(String name)
     {
         this.name = name;
@@ -30,9 +42,12 @@ public class Hello
     public static void main(String a[])
     {
         Human pes = new Human();
+        Human pes1 = new Human(25, "frank");
         pes.setName("sinatra");
         pes.setAge(90);
         System.out.println(pes.getName() + " : " + pes.getAge());
+        System.out.println(pes1.getName() + " : " + pes1.getAge());
+        
        
     }
 }
