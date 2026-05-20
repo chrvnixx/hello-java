b130146ba682a5506de5b7f1edfe5d82013928eb
chrvnixx <emmanueladekola91@gmail.com>
2026-05-20 21:21:23 +0100

    packages

--- Hello.java
+++ Hello.java
@@ -1,46 +1,34 @@
+import java.util.ArrayList;
 
-class Calc
+class A
 {   
-    public int add(int n1, int n2)
+    public void show()
     {
-        return n1 + n2;
+        System.out.println("in a show");
     }
-    public int sub(int n1, int n2)
+    public void config()
     {
-        return n1 - n2;
+        System.out.println("in a config");
     }
+   
+}
+class B extends A
+{   
+   public void show()
+   {
+    System.out.println("in b show");
+   }
+   
 }
 
-class AdvCalc extends Calc
-{
-    public int multi(int n1, int n2)
-    {
-        return n1 * n2;
-    }
-    public int div(int n1, int n2)
-    {
-        return n1 / n2;
-    }
-}
 
-class VeryAdvCalc extends AdvCalc
-{
-    public double power(int n1, int n2)
-    {
-        return Math.pow(n1, n2);
-    }
-}
 
 public class Hello
 {
     public static void main(String a[])
     {
-        VeryAdvCalc obj = new VeryAdvCalc();
-        int r1 = obj.add(10,15);
-        int r2 = obj.sub(90, 18);
-        int r3 = obj.multi(90, 18);
-        double r4 = obj.power(20, 25);
-
-        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+        ArrayList list = new ArrayList();
+        B obj = new B();
+        obj.show();
     }
 }
